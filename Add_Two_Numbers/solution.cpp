1cf45c5a50a3ffec9ee885b57d7c0ee902a8b49f
weixiaoyunryl@163.com <weixiaoyunryl@163.com>
2019-02-20 15:06:36 +0800

    add files

---
+++ Add_Two_Numbers/solution.cpp
@@ -0,0 +1,47 @@
+#include "solution.h"
+
+Solution::Solution()
+{
+
+}
+
+ListNode *Solution::addTwoNumbers(ListNode *l1, ListNode *l2)
+{
+    ListNode* dummyRoot = new ListNode(0);
+    ListNode* ptr = dummyRoot;
+    int sum,a,b = 0;
+
+    for(;l1 != NULL && l2 != NULL;l1 = l1->next, l2 = l2->next)
+    {
+        sum = l1->val + l2->val + b;
+        a = sum % 10;
+        ptr->next = new ListNode(a + b);
+        ptr = ptr->next;
+        b = sum / 10;
+    }
+    for(;l1 != NULL;l1 = l1->next)
+    {
+        sum = l1->val + b;
+        a = sum % 10;
+        ptr->next = new ListNode(a + b);
+        ptr = ptr->next;
+        b = sum / 10;
+    }
+    for(;l2 != NULL;l2 = l2->next)
+    {
+        sum  = l2->val +b;
+        a= sum % 10;
+        ptr->next = new ListNode(a + b);
+        ptr = ptr->next;
+        b = sum / 10;
+    }
+    if(b != 0)
+    {
+        ptr->next = new ListNode(b);
+        ptr = ptr->next;
+    }
+
+    ptr = dummyRoot->next;
+    delete dummyRoot;
+    return ptr;
+}
---
+++ Add_Two_Numbers/solution.h
@@ -0,0 +1,18 @@
+#ifndef SOLUTION_H
+#define SOLUTION_H
+#include <iostream>
+#include <stddef.h>
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution
+{
+public:
+    Solution();
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2);
+};
+
+#endif // SOLUTION_H
